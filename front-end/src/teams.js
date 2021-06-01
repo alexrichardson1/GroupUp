45ea2a50b6fd505d8492d1e7e2734e1f6a3eafef
Ayoob Ahmed <aa1319@ic.ac.uk>
2021-06-01 14:51:34 +0100

    refactor(teams): Change adRequirements to array

--- front-end/src/teams.js
+++ front-end/src/teams.js
@@ -5,7 +5,7 @@ const data = [
     remaining: "1 space left",
     teammates: ["Ayoob Patel", "Alex Duku", "Jaimi Ajmeera"],
     requirements: ["Language - Java", "Timezone - GMT+1"],
-    adRequirements: "Willing to meet in person",
+    adRequirements: ["Willing to meet in person"],
   },
 
   {
@@ -13,7 +13,7 @@ const data = [
     leader: "Alice",
     teammates: ["Jeff Jeffery", "Harry Arnold"],
     requirements: ["Language - Haskell", "Timezone - GMT+1"],
-    adRequirements: "Preferable if speaks spanish",
+    adRequirements: ["Preferable if speaks spanish"],
   },
 ];
 
