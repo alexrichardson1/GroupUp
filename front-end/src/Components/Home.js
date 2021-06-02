0dd6948891a4940cf6ca282256555bddcc8b1c0a
Ayoob Ahmed <aa1319@ic.ac.uk>
2021-06-02 15:30:04 +0100

    feat(Home): Add Home component

---
+++ front-end/src/Components/Home.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome or something like that.</h1>
+      <Link to="/listings">
+        <button className="btn">Join a group</button>
+      </Link>
+      <button className="btn">Create a group</button>
+    </div>
+  );
+};
+
+export default Home;
