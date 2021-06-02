f6410210d3d82d569c3fe3a153cad1fc20943d3c
Ayoob Ahmed <aa1319@ic.ac.uk>
2021-06-02 14:45:36 +0100

    feat(teams.js): Rewrite structure of group object

--- front-end/src/teams.js
+++ front-end/src/teams.js
@@ -1,19 +1,51 @@
 const data = [
   {
     id: 0,
-    leader: "Jason",
-    remaining: "1 space left",
+    leader: "Jason Bourne",
+    maxMembers: 5,
     teammates: ["Ayoob Patel", "Alex Duku", "Jaimi Ajmeera"],
-    requirements: ["Language - Java", "Timezone - GMT+1"],
-    adRequirements: ["Willing to meet in person"],
+    requirements: {
+      codeLanguage: "Java",
+      timezone: 1,
+      spokenLanguage: "English",
+    },
+    adRequirements: "Willing to work in person",
   },
-
   {
     id: 1,
-    leader: "Alice",
+    leader: "Alice Ecila",
+    maxMembers: 5,
     teammates: ["Jeff Jeffery", "Harry Arnold"],
-    requirements: ["Language - Haskell", "Timezone - GMT+1"],
-    adRequirements: ["Preferable if speaks spanish"],
+    requirements: {
+      codeLanguage: "Haskell",
+      timezone: 1,
+      spokenLanguage: "Spanish",
+    },
+    adRequirements: "Proficient in discord",
+  },
+  {
+    id: 3,
+    leader: "Steve Stevenson",
+    maxMember: 4,
+    teammates: ["Barry James", "Phil Mitchell"],
+    requirements: {
+      codeLanguage: "PHP",
+      timezone: -5,
+      spokenLanguage: "English",
+    },
+    adRequirements: "Knowledge in back end technologies",
+  },
+  {
+    id: 4,
+    leader: "Jeff Wetherspoon",
+    remaining: 2,
+    teammates: ["Ellie Lin", "Mohammed Abdi"],
+    requirements: {
+      codeLanguage: "JavaScript",
+      timezone: 1,
+      spokenLanguage: "English",
+    },
+    adRequirements: "",
   },
 ];
 
