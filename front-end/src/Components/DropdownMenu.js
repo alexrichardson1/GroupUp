56ea9d6a28caad7d57100f0db4b557aff883e101
Ayoob Ahmed <aa1319@ic.ac.uk>
2021-06-02 16:46:49 +0100

    feature(DropdownMenu): Dropdown component

---
+++ front-end/src/Components/DropdownMenu.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import data from "../Teams";
+
+const DropdownMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const getGroups = () => {
+    //     let groupIds = [];
+    //     data.forEach((group) => groupIds.push(group[projectId]));
+    //     let uniqueIds = [...new Set(groupIds)];
+    //     return uniqueIds;
+  };
+
+  return;
+  <div>
+    <button onClick={() => setOpen(!open)}>Select Project</button>
+    {open && getGroups().map((id) => <a href="/listings">{id}</a>)}
+  </div>;
+};
+
+export default DropdownMenu;
