0e0fa63d0c69742d19a0be4c39487f43102a45cc
Ayoob Ahmed <aa1319@ic.ac.uk>
2021-06-03 11:35:38 +0100

    feat(DetailedGroup.js): Add useParams to get right group

--- front-end/src/Components/DetailedGroup.js
+++ front-end/src/Components/DetailedGroup.js
@@ -1,5 +1,11 @@
-const DetailedGroup = (props) => {
-  return <h3>This is a detailed group</h3>;
+import { Link, useParams } from "react-router-dom";
+import data from "../Teams";
+
+const DetailedGroup = () => {
+  const { id } = useParams();
+  const { leader, maxMembers, teammates, requirements, adRequirements } =
+    data[parseInt(id)];
+  return <h3>This is a detailed group {leader}</h3>;
 };
 
 export default DetailedGroup;
