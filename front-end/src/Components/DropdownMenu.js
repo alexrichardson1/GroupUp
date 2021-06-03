4333de7451ddbe28a125d9206d6a1bd543d0e93e
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-03 13:10:02 +0100

    feat(DropdownMenu): Add offclick functinailty
    A click off the dropdown will close the dropdown options

--- front-end/src/Components/DropdownMenu.js
+++ front-end/src/Components/DropdownMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./styles.css";
 
 // import data from "../Teams";
@@ -24,17 +24,29 @@ import "./styles.css";
 
 export default function DropdownMenu({ promt, options, value, onChange }) {
   const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", close);
+    return () => document.removeEventListener("click", close);
+  }, []);
+
+  function close(event) {
+    setOpen(event && event.target === ref.current);
+  }
 
   return (
     <div className="dropdown">
       <div className="control" onClick={() => setOpen((prev) => !prev)}>
-        <div className="selected-value">{value ? value : promt}</div>
+        <div className="selected-value" ref={ref}>
+          {value ? value : promt}
+        </div>
         <div className={`arrow ${open ? "open" : null}`} />
       </div>
       <div className={`options ${open ? "open" : null}`}>
         {options.map((option) => (
           <div
-            className="option"
+            className={`option ${value === option ? "selected" : null}`}
             onClick={() => {
               onChange(option);
               setOpen(false); // automatically close dropdown
