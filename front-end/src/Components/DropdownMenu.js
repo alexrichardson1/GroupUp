e3f90f7dd8a04f2a11812865284e6b3fa2d7e186
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-03 15:07:37 +0100

    chore(DropdownMenu): Remove commented code

--- front-end/src/Components/DropdownMenu.js
+++ front-end/src/Components/DropdownMenu.js
@@ -1,27 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./styles.css";
 
-// import data from "../Teams";
-
-// const DropdownMenu = () => {
-//   const [open, setOpen] = useState(false);
-
-//   const getGroups = () => {
-//     //     let groupIds = [];
-//     //     data.forEach((group) => groupIds.push(group[projectId]));
-//     //     let uniqueIds = [...new Set(groupIds)];
-//     //     return uniqueIds;
-//   };
-
-//   return null;
-//   // <div>
-//   //   <button onClick={() => setOpen(!open)}>Select Project</button>
-//   //   {open && getGroups().map((id) => <a href="/listings">{id}</a>)}
-//   // </div>;
-// };
-
-// // export default DropdownMenu;
-
 export default function DropdownMenu({
   promt,
   options,
