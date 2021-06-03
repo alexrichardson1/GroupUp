81f0b88b7d095614e53b054528d90970117a7cd8
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-03 12:52:54 +0100

    feat(SelectProject): Define props for Year

--- front-end/src/Components/SelectProject.js
+++ front-end/src/Components/SelectProject.js
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
 import years from "./years.json";
 import projects from "./projects.json";
 
 const SelectProject = (props) => {
+  const [value, setValue] = useState(null);
+
   return (
     <div>
       <body>
@@ -11,8 +14,14 @@ const SelectProject = (props) => {
         the best team for you.
       </body>
       <h1>Imperial</h1>
-      Year <DropdownMenu promt="Select year" options={years} />
-      Project <DropdownMenu promt="Select project" options={projects} />
+      Year{" "}
+      <DropdownMenu
+        promt="Select year"
+        options={years}
+        value={value}
+        onChange={(val) => setValue(val)}
+      />
+      {/* Project <DropdownMenu promt="Select project" options={projects} /> */}
       <Link to="/listings">
         <button className="btn">Find your group</button>
       </Link>
