24cfbc498be8a61f6a69129322b741c6b460b5e1
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-04 08:19:39 +0100

    test(App.test): Add 'create a group' link test

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -6,3 +6,9 @@ test("Renders 'Join a group' link", () => {
   const linkElement = screen.getByText(/Join a group/i);
   expect(linkElement.textContent).toBe("Join a group");
 });
+
+test("Renders 'Create a group' link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Create a group/i);
+  expect(linkElement.textContent).toBe("Create a group");
+});
