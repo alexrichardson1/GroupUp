4177eb48955afe4d595829ed2c8d95dc61cc22c8
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-04 10:02:49 +0100

    test(App.test): Add page link test

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import App from "./App";
 
 test("Renders 'Join a group' link", () => {
@@ -11,4 +11,14 @@ test("Renders 'Create a group' link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Create a group/i);
   expect(linkElement.textContent).toBe("Create a group");
+});
+
+test("Clicking join a group gives 'select group' page", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Join a group/i);
+  fireEvent.click(linkElement);
+  const text = screen.getByText(/Please give us/);
+  expect(text.textContent).toBe(
+    "Please give us more details about your project so we can help you find the best team for you."
+  );
 });
