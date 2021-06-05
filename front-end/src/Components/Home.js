39672355b3f648c356f306e1cc73c7e8089fd050
Hussein, Mazen <mah319@ic.ac.uk>
2021-06-05 15:26:47 +0100

    feat(Home.js): home page to bootstrap

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap'
+import { Button, Jumbotron } from 'react-bootstrap'
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to GroupUp, the easy tool for helping you find a competition group.</h1>
-      <Link to="/selection">
-        <button className="btn">Get Started</button>
-      </Link>
+      <Jumbotron>
+        <h1>Welcome to GroupUp</h1>
+        <p>
+          An easy tool for helping you find a group for your next Hackathon!
+        </p>
+        <LinkContainer to="/selection">
+          <Button>Get Started</Button>
+        </LinkContainer>
+      </Jumbotron>
+
     </div>
   );
 };
