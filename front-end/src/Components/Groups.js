66f66adbe89cce812756ce9b1231894822de24dd
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-07 19:05:32 +0100

    refactor(Groups): Add backend API call

--- front-end/src/Components/Groups.js
+++ front-end/src/Components/Groups.js
@@ -1,18 +1,44 @@
 import Group from "./Group";
+import { Component } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
-const Groups = ({ groups }) => {
-  return (
-    <div>
-      <h3>2 Groups looking for members in Project X</h3>
-      <Link to="/createGroup">
-        <button className="btn">Advertise my group!</button>
-      </Link>
-      {groups.map((group) => (
-        <Group group={group} key={group.id} />
-      ))}
-    </div>
-  );
-};
+async function getGroups() {
+  var result = [];
+  await axios
+    .get("http://localhost:5000/group")
+    .then((res) => {
+      const groups = res.data;
+      console.log(groups);
+      result = groups;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return result;
+}
 
-export default Groups;
+export default class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { groups: [] };
+  }
+
+  async componentDidMount() {
+    this.setState({ groups: await getGroups() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>2 Groups looking for members in Project X</h3>
+        <Link to="/createGroup">
+          <button className="btn">Advertise my group!</button>
+        </Link>
+        {this.state.groups.map((group) => (
+          <Group group={group} key={group.id} />
+        ))}
+      </div>
+    );
+  }
+}
