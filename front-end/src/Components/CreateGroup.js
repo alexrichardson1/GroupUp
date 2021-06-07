ea1571966574dfa88bb782b61c9efc5d665ed520
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-07 19:17:30 +0100

    feat(CreateGroup): Add template form

--- front-end/src/Components/CreateGroup.js
+++ front-end/src/Components/CreateGroup.js
@@ -1,5 +1,54 @@
-const CreateGroup = () => {
-  return <h1>Create your group</h1>;
-};
+import React, { Component } from "react";
+import axios from "axios";
+import "./styles.css";
 
-export default CreateGroup;
+async function addGroup(data) {
+  var result = {};
+  await axios
+    .post(`"http://localhost:5000/group/add`, data)
+    .then((res) => {
+      const group = res.data;
+      console.log(group);
+      result = group;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return result;
+}
+
+export default class CreateGroup extends Component {
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const title = this.title.value;
+    const tags = this.tags.value;
+    const info = { title: title, tags: [tags] };
+    await addGroup(info);
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <div className="formWrap">
+        <div className="formContent">
+          <form className="form" onSubmit={this.onSubmit}>
+            <h1 className="formH1">Enter Session Details</h1>
+            <input
+              type="text"
+              className="formInput"
+              placeholder="Title"
+              ref={(input) => (this.title = input)}
+            />
+            <input
+              type="text"
+              className="formInput"
+              placeholder="Tags"
+              ref={(input) => (this.tags = input)}
+            />
+            <button className="createButton">Create Session</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
