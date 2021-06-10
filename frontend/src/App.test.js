72b97bef7141b502ea36a5489797fe552639a768
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-10 17:39:40 +0100

    test(App.test): Change desired text

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,8 +3,8 @@ import App from "./App";
 
 test("Renders 'Join a group' link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Get Started/i);
-  expect(linkElement.textContent).toBe("Get Started");
+  const linkElement = screen.getByText(/Log In!/i);
+  expect(linkElement.textContent).toBe("Log In!");
 });
 
 test("Clicking join a group gives 'select group' page", () => {
@@ -12,7 +12,5 @@ test("Clicking join a group gives 'select group' page", () => {
   const linkElement = screen.getByText(/Get Started/i);
   fireEvent.click(linkElement);
   const text = screen.getByText(/List of Competitions Available/);
-  expect(text.textContent).toBe(
-    "List of Competitions Available"
-  );
+  expect(text.textContent).toBe("List of Competitions Available");
 });
