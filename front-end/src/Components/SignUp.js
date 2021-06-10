e6b242b22e424b68af7f4f26f87a6a055ad66533
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-10 12:42:40 +0100

    refactor(SignUp): Use bootstrap

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -1,34 +1,44 @@
 import React, { Component } from "react";
 import { LinkContainer } from "react-router-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 class SignUp extends Component {
   render() {
     return (
-      <form>
+      <div>
         <h2>Create an Account!</h2>
-        <fieldset>
-          <ul>
-            <li>
-              <label for="username">Username:</label>
-              <input type="text" id="username" required />
-            </li>
-            <li>
-              <label for="email">Email:</label>
-              <input type="email" id="email" required />
-            </li>
-            <li>
-              <label for="password">Password:</label>
-              <input type="password" id="password" required />
-            </li>
-          </ul>
-        </fieldset>
-        <LinkContainer to="/home">
-          <button>Submit</button>
-        </LinkContainer>
-        <LinkContainer to="/">
-          <button type="button">Have an Account?</button>
-        </LinkContainer>
-      </form>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <LinkContainer to="/">
+            <Button>Login</Button>
+          </LinkContainer>
+          <LinkContainer to="/signup">
+            <Button type="button">Create an Account</Button>
+          </LinkContainer>
+          <LinkContainer to="/forgotpassword">
+            <Button>Forgot Password?</Button>
+          </LinkContainer>
+        </Form>
+      </div>
+      //     </ul>
+      //   </fieldset>
+      //   <LinkContainer to="/home">
+      //     <button>Submit</button>
+      //   </LinkContainer>
+      //   <LinkContainer to="/">
+      //     <button type="button">Have an Account?</button>
+      //   </LinkContainer>
+      // </form>
     );
   }
 }
