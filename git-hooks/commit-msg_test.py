54ec995ca434e710c27e4e56b6c00f01a157a7cb
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-12 15:29:17 +0100

    test(commit-msg_test): Add commit-msg_test.py

---
+++ git-hooks/commit-msg_test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import unittest
+from pyfakefs.fake_filesystem_unittest import TestCase
+import importlib
+hook = importlib.import_module("commit-msg")
+
+
+class ExitFailure(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_exit_failure(self):
+        hook.exit_failure()
+
+
+@unittest.expectedFailure
+class FollowsConventionFailure(unittest.TestCase):
+    def test_follows_convention_empty_message(self):
+        hook.follows_convention("")
+
+    @unittest.skip("FIXME")
+    def test_follows_convention_empty_scope(self):
+        hook.follows_convention("feat(): Add func")
+
+    def test_follows_convention_verbose_message(self):
+        hook.follows_convention(
+            "feat(foo): This is a really long commit message and should fail")
+
+    def test_follows_convention_empty_message_trailing_punctuation(self):
+        hook.follows_convention("fix(foo): No trailing punctuation!")
+        hook.follows_convention("fix(foo): No trailing punctuation?")
+        hook.follows_convention("fix(foo): No trailing punctuation...")
+        hook.follows_convention("fix(foo): No trailing punctuation.")
+        hook.follows_convention("fix(foo): No trailing punctuation:")
+
+
+class FollowsConvention(unittest.TestCase):
+    def test_follows_convention_allowed_types(self):
+        hook.follows_convention("feat(foo): Add feature")
+        hook.follows_convention("fix(foo): Remove syntax error")
+        hook.follows_convention("style(foo): Format code")
+        hook.follows_convention("refactor(foo): Change feature")
+        hook.follows_convention("perf(foo): Add concurreny")
+        hook.follows_convention("test(foo): Change feature")
+        hook.follows_convention("docs(README): Add section to README")
+        hook.follows_convention("chore(foo): Organise file structure")
+        hook.follows_convention("build(foo): Add dependency")
+        hook.follows_convention("ci(Dockerfile): Add RUN command")
+
+
+class UpdateCommitMsg(TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+        self.file_path = "/foo/bar/commit_msg.txt"
+
+    def test_update_commit_msg_following_convention_unchanged(self):
+        commit_msg = "feat(foo): Add feature"
+        self.fs.create_file(self.file_path, contents=commit_msg)
+        self.assertTrue(hook.update_commit_msg(self.file_path) == [commit_msg])
+
+    def test_update_commit_msg_trailing_space_removed(self):
+        commit_msg = "     feat(foo): Add feature"
+        self.fs.create_file(self.file_path, contents=commit_msg)
+        self.assertTrue(
+            hook.update_commit_msg(
+                self.file_path) == [
+                commit_msg.lstrip()])
+
+    def test_update_commit_msg_replaces_co_authors(self):
+        commit_msg = """feat(foo): Add feature
+
+        W/ MH"""
+        self.fs.create_file(self.file_path, contents=commit_msg)
+        self.assertTrue(
+            hook.update_commit_msg(
+                self.file_path) == [
+                'feat(foo): Add feature\n',
+                '',
+                'Co-authored-by: Hussein, Mazen <mazen.hussein319@imperial.ac.uk>'])
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
