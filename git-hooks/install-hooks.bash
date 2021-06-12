29a12fb74cc820b6036e58a1af260620178daf84
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-12 15:31:27 +0100

    chore(install-hooks): Update symlinks

--- git-hooks/install-hooks.bash
+++ git-hooks/install-hooks.bash
@@ -2,11 +2,16 @@
 
 # Run the command `chmod +x ./install-hooks.bash` in the scripts directory
 
+HOOK_DIR=git-hooks
 GIT_DIR=$(git rev-parse --git-dir)
 
-chmod +x commit-msg.py
+chmod +x pre-commit.py commit-msg.py pre-push.bash post-checkout.py
 
 echo "Installing hooks..."
 # symlinks to the pre-commit script
-ln -sf $GIT_DIR/../git-hooks/commit-msg.py $GIT_DIR/hooks/commit-msg
+git config --local commit.template $HOOK_DIR/commit_template.txt
+ln -sf $GIT_DIR/../$HOOK_DIR/pre-commit.py $GIT_DIR/hooks/pre-commit
+ln -sf $GIT_DIR/../$HOOK_DIR/commit-msg.py $GIT_DIR/hooks/commit-msg
+ln -sf $GIT_DIR/../$HOOK_DIR/pre-push.bash $GIT_DIR/hooks/pre-push
+ln -sf $GIT_DIR/../$HOOK_DIR/post-checkout.py $GIT_DIR/hooks/post-checkout
 echo "Done!"
