2e74bb5927af485454ee08707ce92d4d47eb23d6
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-13 18:32:11 +0100

    refactor(Groups): Call backend API

--- frontend/src/components/Groups.js
+++ frontend/src/components/Groups.js
@@ -1,115 +1,91 @@
+import { Component } from "react";
 import Group from "components/Group";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Filter from "components/Filter";
-import hackathons from "data/projects.json";
 import Navbar from "components/NavBar";
+import axios from "axios";
 
-const Groups = ({ allGroups }) => {
-  const { id } = useParams();
-  const hackathonId = parseInt(id);
-  const hackathonName = hackathons.filter((proj) => proj.id === hackathonId)[0]
-    .name;
-  const hackathonGroups = allGroups.filter(
-    (group) => group.projectId === hackathonId
-  );
-  const hackathonReqs = hackathons.filter((proj) => proj.id === hackathonId)[0]
-    .requirements;
+export default class GroupsNew extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: [],
+      projectName: "",
+    };
+  }
 
-  const [filteredGroups, setFilteredGroups] = useState([...hackathonGroups]);
-  const [activeFilters, setActiveFilters] = useState(new Map());
+  async getGroups() {
+    var result = [];
+    await axios
+      .post("http://localhost:5000/group/hackathon", {
+        hackathonid: this.props.id,
+      })
 
-  useEffect(() => {
-    for (const [k, v] of activeFilters.entries()) {
-      setFilteredGroups((f) =>
-        f.filter((group) => group.requirements[k] === v)
-      );
-    }
+      .then((res) => {
+        const groups = res.data;
+        result = groups;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return result;
+  }
 
-    console.log("Active Filters after: ");
+  async getProjectName() {
+    var result = "";
+    await axios
+      .get("http://localhost:5000/project")
+      .then((res) => {
+        const projects = res.data;
+        result = projects.filter((proj) => proj.id == this.props.id)[0].name;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    return result;
+  }
 
-    for (const [k, v] of activeFilters.entries()) {
-      console.log(k, v);
-    }
-    console.log("--------------------");
-  }, [activeFilters]);
+  async componentDidMount() {
+    this.setState({ groups: await this.getGroups() });
+    this.setState({ projectName: await this.getProjectName() });
+  }
 
-  const updateFilter = (key, value) => {
-    setActiveFilters((prev) => new Map(prev).set(key, value));
-  };
-
-  const removeFilter = (key) => {
-    setActiveFilters((prev) => {
-      const newState = new Map(prev);
-      newState.delete(key);
-      return newState;
-    });
-  };
-
-  /* This will filter groups on all "active filters", plus the new filter specified. */
-  const filterGroupsOnReq = (reqName, reqVal) => {
-    setFilteredGroups(hackathonGroups);
-    console.log("--------------------");
-    console.log(
-      "attempting filter of {" + reqName + "} and with value {" + reqVal + "}"
+  render() {
+    return (
+      <div>
+        <Navbar renderBool={[true, true, true, false]} create={false} />
+        <Container>
+          <Row>
+            <h3>
+              {/* {filteredGroups.length} Groups looking for members in:  */}
+              {this.state.projectName}
+            </h3>
+          </Row>
+          <Row>
+            <LinkContainer to={`/createGroup/${this.props.id}`}>
+              <Button>Advertise my group!</Button>
+            </LinkContainer>
+          </Row>
+          <Row>
+            {/* <Col>
+              {hackathonReqs.map((req) => (
+                <Filter
+                  requirementName={req}
+                  requirementsList={getAllReqVars(req)}
+                  filterFunction={filterGroupsOnReq}
+                  resetFunction={deleteFilter}
+                />
+              ))}
+            </Col> */}
+            <Col>
+              {this.state.groups.map((group) => (
+                <Group group={group} key={group.id} />
+              ))}
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
-    console.log("Active Filters before: ");
-    updateFilter(reqName, reqVal);
-  };
-
-  const getAllReqVars = (req) => {
-    const vars = new Set();
-    hackathonGroups.forEach((group) => vars.add(group.requirements[req]));
-    return [...vars];
-  };
-
-  const deleteFilter = (reqName) => {
-    removeFilter(reqName);
-    const newFilteredGroups = hackathonGroups;
-    for (const [k, v] of activeFilters.entries()) {
-      newFilteredGroups.filter((group) => group.requirements[k] === v);
-    }
-    setFilteredGroups(newFilteredGroups);
-  };
-
-  return (
-    <div>
-      <Navbar renderBool={[true, true, true, false]} create={false} />
-      <Container>
-        <Row>
-          <h3>
-            {filteredGroups.length} Groups looking for members in: 
-            {hackathonName}
-          </h3>
-        </Row>
-        <Row>
-          <LinkContainer to="/createGroup">
-            <Button>Advertise my group!</Button>
-          </LinkContainer>
-        </Row>
-        <Row>
-          <Col>
-            {hackathonReqs.map((req) => (
-              <Filter
-                requirementName={req}
-                requirementsList={getAllReqVars(req)}
-                filterFunction={filterGroupsOnReq}
-                resetFunction={deleteFilter}
-              />
-            ))}
-          </Col>
-          <Col>
-            {filteredGroups.map((group) => (
-              <Group group={group} key={group.id} />
-            ))}
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
-
-export default Groups;
+  }
+}
