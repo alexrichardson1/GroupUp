bf4b588f742fc29e615447d35383a3c4692324fe
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-13 18:33:26 +0100

    refactor(CreateGroup): Call backend API

--- frontend/src/components/CreateGroup.js
+++ frontend/src/components/CreateGroup.js
@@ -21,26 +21,72 @@ async function addGroup(data) {
 }
 
 class CreateGroup extends Component {
-  onSubmit = async (event) => {
-    event.preventDefault();
-    const title = this.title.value;
-    const tags = this.tags.value;
-    const info = { title: title, tags: [tags] };
-    this.goBack = this.goBack.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      leader: "",
+      maxmembers: 0,
+      teammates: "",
+      timezone: "",
+      language: "",
+      nationality: "",
+      adrequirements: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /* Functions to handle form submission */
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
+    this.setState({
+      [target.name]: value,
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const info = {
+      leader: this.state.leader,
+      maxmembers: this.state.maxmembers,
+      teammates: this.state.teammates.split(", "),
+      requirements: [
+        this.state.timezone,
+        this.state.language,
+        this.state.nationality,
+      ],
+      adrequirements: this.state.adrequirements,
+      projectid: this.props.id,
+    };
+    console.log(info);
     await addGroup(info);
     window.location.reload();
   };
 
+  // history = useHistory();
+
   render() {
-    //let history = useHistory();
     return (
       <div>
-        <NavBar renderBool={[true, true, true, true]} create={true} />
+        <NavBar
+          renderBool={[true, true, true, true]}
+          create={true}
+          id={this.props.id}
+        />
         <h1>Create your group</h1>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formLeaderName">
             <Form.Label>Leader's Full Name</Form.Label>
-            <Form.Control type="text" placeholder="Leader's Full Name" />
+            <Form.Control
+              type="text"
+              placeholder="Leader's Full Name"
+              name="leader"
+              value={this.state.leader}
+              onChange={this.handleInputChange}
+            />
             <Form.Text className="text-muted">
               This can really be any one of your names, it's just the name your
               group will be advertised under.
@@ -49,7 +95,13 @@ class CreateGroup extends Component {
 
           <Form.Group controlId="formOtherMembers">
             <Form.Label>Other Members</Form.Label>
-            <Form.Control type="text" placeholder="All other members go here" />
+            <Form.Control
+              type="text"
+              placeholder="All other members go here"
+              name="teammates"
+              value={this.state.teammates}
+              onChange={this.handleInputChange}
+            />
           </Form.Group>
 
           <InputGroup className="mb-3">
@@ -60,6 +112,9 @@ class CreateGroup extends Component {
               placeholder="e.g. +5 = GMT+5"
               aria-label="Username"
               aria-describedby="basic-addon1"
+              name="timezone"
+              value={this.state.timezone}
+              onChange={this.handleInputChange}
             />
           </InputGroup>
 
@@ -73,6 +128,9 @@ class CreateGroup extends Component {
               placeholder="e.g. Java"
               aria-label="Username"
               aria-describedby="basic-addon1"
+              name="language"
+              value={this.state.language}
+              onChange={this.handleInputChange}
             />
           </InputGroup>
 
@@ -84,17 +142,32 @@ class CreateGroup extends Component {
               placeholder="e.g. United Kingdom"
               aria-label="Username"
               aria-describedby="basic-addon1"
+              name="nationality"
+              value={this.state.nationality}
+              onChange={this.handleInputChange}
             />
           </InputGroup>
 
           <Form.Group controlId="formOtherMembers">
             <Form.Label>Other Requirements</Form.Label>
-            <Form.Control type="text" placeholder="" />
+            <Form.Control
+              type="text"
+              placeholder=""
+              name="adrequirements"
+              value={this.state.adrequirements}
+              onChange={this.handleInputChange}
+            />
           </Form.Group>
 
           <Form.Group controlId="formMembersNeeded">
             <Form.Label>Members needed</Form.Label>
-            <Form.Control as="select" custom>
+            <Form.Control
+              as="select"
+              custom
+              name="maxmembers"
+              value={this.state.maxmembers}
+              onChange={this.handleInputChange}
+            >
               <option>1</option>
               <option>2</option>
               <option>3</option>
@@ -105,7 +178,7 @@ class CreateGroup extends Component {
           {/* <LinkContainer to="/listing//">
           <Button>Go Back</Button>
         </LinkContainer> */}
-          <Button onClick={() => this.props.history.goBack()}>Go Back</Button>
+          {/* <Button onClick={() => history.goBack()}>Go Back</Button> */}
           <Button variant="primary" type="submit">
             Submit
           </Button>
