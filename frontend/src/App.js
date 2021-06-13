733a790f6cd8057cd181c604153d66c70cad27ac
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-13 18:33:04 +0100

    refactor(App.js): Add id to createGroup/

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,24 @@ import Groups from "components/Groups";
 import Home from "components/Home";
 import SelectProject from "components/SelectProject";
 import DetailedGroup from "components/DetailedGroup";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "App.css";
 import CreateGroup from "components/CreateGroup";
 import Login from "components/auth/Login";
 import SignUp from "components/auth/SignUp";
 import ForgotPassword from "components/auth/ForgotPassword";
-import data from "Teams";
+// import data from "Teams";
+
+function GroupsFunc() {
+  const { id } = useParams();
+  return <Groups id={id} />;
+}
+
+function CreateGroupFunc() {
+  const { id } = useParams();
+  return <CreateGroup id={id} />;
+}
 
 const App = () => {
   return (
@@ -21,13 +31,10 @@ const App = () => {
         <Route path="/forgotpassword" exact component={ForgotPassword}></Route>
         <Route path="/home" exact component={Home}></Route>
         <Route path="/selection" exact component={SelectProject}></Route>
-        <Route path="/listings" exact render={() => <Groups />}></Route>
+        {/* <Route path="/listings" exact render={() => <Groups />}></Route> */}
         <Route path="/group/:id" children={<DetailedGroup />}></Route>
-        <Route
-          path="/listing/:id"
-          children={<Groups allGroups={data} />}
-        ></Route>
-        <Route path="/createGroup" exact component={CreateGroup}></Route>
+        <Route path="/listing/:id" children={<GroupsFunc />}></Route>
+        <Route path="/createGroup/:id" children={<CreateGroupFunc />}></Route>
       </div>
     </Router>
   );
