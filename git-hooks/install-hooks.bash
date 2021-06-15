9ac86492ed5edc764cc3fb04b60ba4b1d1dbc497
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-15 22:00:29 +0100

    chore(install-hooks): Remove post-checkout link

--- git-hooks/install-hooks.bash
+++ git-hooks/install-hooks.bash
@@ -13,5 +13,4 @@ git config --local commit.template $HOOK_DIR/commit_template.txt
 ln -sf $GIT_DIR/../$HOOK_DIR/pre-commit.py $GIT_DIR/hooks/pre-commit
 ln -sf $GIT_DIR/../$HOOK_DIR/commit-msg.py $GIT_DIR/hooks/commit-msg
 ln -sf $GIT_DIR/../$HOOK_DIR/pre-push.bash $GIT_DIR/hooks/pre-push
-ln -sf $GIT_DIR/../$HOOK_DIR/post-checkout.py $GIT_DIR/hooks/post-checkout
 echo "Done!"
