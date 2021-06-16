81683899a18c521844abe4cbff0d194f2d1b7147
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-16 18:16:11 +0100

    feat(Login.js): Push name to context for login Co-authored-by: Hussein, Mazen <mazen.hussein319@imperial.ac.uk>

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,39 +1,67 @@
-import React, { Component } from "react";
-import { LinkContainer } from "react-router-bootstrap";
+import React, { useState } from "react";
+// import { LinkContainer } from "react-router-bootstrap";s
 import { Form, Button } from "react-bootstrap";
 import NavBar from "components/NavBar";
+import { UserContext } from "components/auth/UserContext";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
-class Login extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar renderBool={[false, false, false, false]} loginPage={0} />
-        <h2>Login!</h2>
-        <Form>
-          <Form.Group>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              This email will be used to advertise your group.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group>
+const Login = () => {
+  const [userEmail, setUserEmail] = useState("");
+  // const { value, setValue } = useContext(UserContext);
+  const { setValue } = useContext(UserContext);
+
+  const history = useHistory();
+
+  /* Functions to handle form submission */
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
+    setUserEmail(value);
+    console.log("gelllo");
+  };
+
+  const handleSubmit = () => {
+    setValue(userEmail);
+    history.push("/home");
+  };
+
+  return (
+    <div>
+      <NavBar renderBool={[false, false, false, false]} loginPage={0} />
+      <h2>Login!</h2>
+      <Form>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="you@email.com"
+            name="userEmail"
+            value={userEmail}
+            onChange={handleInputChange}
+          />
+          <Form.Text className="text-muted">
+            Leader is just the point of contact, it can really be any group
+            member.
+          </Form.Text>
+        </Form.Group>
+        {/* <Form.Group>
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <LinkContainer to="/home">
-            <Button>Login</Button>
-          </LinkContainer>
-          <LinkContainer to="/signup">
+          </Form.Group> */}
+        {/* <LinkContainer to="/home"> */}
+        <Button onClick={handleSubmit}>Login</Button>
+        {/* </LinkContainer> */}
+        {/* <LinkContainer to="/signup">
             <Button>Create an Account</Button>
           </LinkContainer>
           <LinkContainer to="/forgotpassword">
             <Button>Forgot Password?</Button>
-          </LinkContainer>
-        </Form>
-      </div>
-    );
-  }
-}
+          </LinkContainer> */}
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
