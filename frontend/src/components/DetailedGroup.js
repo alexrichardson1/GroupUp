14bc380592bbdc5c66ad1f6bf9d15a2b9386c60c
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-16 19:12:09 +0100

    build(DetailGroup): Change to absolute import Co-authored-by: Ahmed, Ayoob <ayoob.ahmed1319@imperial.ac.uk>

--- frontend/src/components/DetailedGroup.js
+++ frontend/src/components/DetailedGroup.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Button, ListGroup, Table } from "react-bootstrap";
 import NavBar from "components/NavBar";
 import axios from "axios";
-import { config } from "../Constants";
+import { config } from "Constants";
 
 const DetailedGroup = () => {
   const { id } = useParams();
