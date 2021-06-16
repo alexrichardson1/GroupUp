9bc68c780d9057f1943b499f746aaaecf0dc089e
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-16 12:52:57 +0100

    refactor(SelectProject): Add table for hackathons Co-authored-by: Hussein, Mazen <mazen.hussein319@imperial.ac.uk>

--- frontend/src/components/SelectProject.js
+++ frontend/src/components/SelectProject.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, Table, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { useHistory } from "react-router-dom";
 import NavBar from "components/NavBar";
 import axios from "axios";
@@ -30,29 +31,41 @@ const SelectProject = () => {
     getProjects();
   }, []);
 
+  const testFunc = () => {
+    console.log("hellloooo");
+    console.log(projects);
+  };
+
   return (
     <div>
       <NavBar renderBool={[true, true, false, false]} create={false} />
       <h1>List of Competitions Available</h1>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Select Project
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Hackathon Name</th>
+            <th>Description</th>
+            <th>Start Date</th>
+            <th>Length (hours)</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
           {projects.map((proj) => (
-            <Dropdown.Item
-              key={proj.id}
-              onSelect={() => handleProjectClick(proj.id)}
-            >
-              {proj.name}
-            </Dropdown.Item>
+            <tr>
+              <td>{proj.name}</td>
+              <td>{proj.description}</td>
+              <td>{proj.date.substring(0, 10)}</td>
+              <td>{proj.hours}</td>
+              <td>
+                <LinkContainer to={`/listing/${proj.id}`}>
+                  <Button>View Listings</Button>
+                </LinkContainer>
+              </td>
+            </tr>
           ))}
-          {
-            //Add href="#/your-action" when action available
-          }
-        </Dropdown.Menu>
-      </Dropdown>
+        </tbody>
+      </Table>
       {/* <LinkContainer to="/listings">
         <Button>Show groups</Button>
       </LinkContainer> */}
