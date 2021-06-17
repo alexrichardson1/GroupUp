e796f0c5a2e4f6aef1a32e892130790a235bd140
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-17 11:40:50 +0100

    refactor(Home): Change Home into a function Co-authored-by: Ahmed, Ayoob <ayoob.ahmed1319@imperial.ac.uk>

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,85 +1,70 @@
-import { Component } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { Button, Jumbotron, Card, ListGroup } from "react-bootstrap";
 import NavBar from "components/NavBar";
 import axios from "axios";
 import { config } from "Constants";
+import { useState, useEffect, useContext } from "react";
 import { UserContext } from "components/auth/UserContext";
-// import Group from "components/Group";
 
-export default class Home extends Component {
-  static contextType = UserContext;
+const Home = () => {
+  const [groups, setGroups] = useState([]);
+  const { value } = useContext(UserContext);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      groups: [],
-      project: "",
-      requirements: [],
-      filteredGroups: [],
-      activeFilters: new Map(),
-    };
-  }
-
-  async getGroups() {
-    var result = [];
-    await axios
-      .get(`${config.API_URL}/group`)
-      .then((res) => {
-        const groups = res.data;
-        result = groups;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    return result;
-  }
-
-  async componentDidMount() {
-    this.setState({ groups: await this.getGroups() });
-  }
-
-  filterGroupsOnName = () => {
-    // const { user, setUser } = this.context;
-    // TODO: change James Smith to value
-    return this.state.groups.filter(
+  const filterGroupsOnName = () => {
+    return groups.filter(
       (group) =>
-        group.leader === "James Smith" ||
-        group.teammates.some((member) => member === "James Smith")
+        group.leader === value ||
+        group.teammates.some((member) => member === value)
     );
   };
 
-  render() {
-    return (
-      <div>
-        <NavBar renderBool={[true, false, false, false]} create={false} />
-        <Jumbotron>
-          <h1 className="title">Welcome to GroupUp</h1>
-          <div>
-            An easy tool for helping you find a group for your next Hackathon!
-          </div>
-          <LinkContainer to="/selection">
-            <Button>Get Started</Button>
-          </LinkContainer>
-          <h3 className="groupsHome">Groups you're already in.</h3>
-          <div>
-            {this.filterGroupsOnName().map((group) => (
-              <Card.Body>
-                <Card.Title>{group.leader}'s Group</Card.Title>
-                <Card.Text>
-                  <ListGroup.Item variant="dark">Other Members:</ListGroup.Item>
-                  {group.teammates.map((teammate) => (
-                    <ListGroup.Item variant="flush">{teammate}</ListGroup.Item>
-                  ))}
-                </Card.Text>
-                <LinkContainer to={`/group/${group.id}`}>
-                  <Button>More Info</Button>
-                </LinkContainer>
-              </Card.Body>
-            ))}
-          </div>
-        </Jumbotron>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    const getGroups = async () => {
+      await axios
+        .get(`${config.API_URL}/group/`)
+        .then((res) => {
+          const group = res.data;
+          setGroups(group);
+          console.log(group);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    getGroups();
+  }, []);
+
+  return (
+    <div>
+      <NavBar renderBool={[true, false, false, false]} create={false} />
+      <Jumbotron>
+        <h1 className="title">Welcome to GroupUp</h1>
+        <div>
+          An easy tool for helping you find a group for your next Hackathon!
+        </div>
+        <LinkContainer to="/selection">
+          <Button>Get Started</Button>
+        </LinkContainer>
+        <h3 className="groupsHome">Groups you're already in.</h3>
+        <div>
+          {filterGroupsOnName().map((group) => (
+            <Card.Body>
+              <Card.Title>{group.leader}'s Group</Card.Title>
+              <Card.Text>
+                <ListGroup.Item variant="dark">Other Members:</ListGroup.Item>
+                {group.teammates.map((teammate) => (
+                  <ListGroup.Item variant="flush">{teammate}</ListGroup.Item>
+                ))}
+              </Card.Text>
+              <LinkContainer to={`/group/${group.id}`}>
+                <Button>More Info</Button>
+              </LinkContainer>
+            </Card.Body>
+          ))}
+        </div>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Home;
