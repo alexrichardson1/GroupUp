c7886680da9c9b1a47174e1f03911ffa73adba56
R, Alex <alex.richardson19@imperial.ac.uk>
2021-06-17 14:13:09 +0100

    feat(Login): Update user last login
    Co-authored-by: Hussein, Mazen <mazen.hussein319@imperial.ac.uk>
    Co-authored-by: Ahmed, Ayoob <ayoob.ahmed1319@imperial.ac.uk>

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -4,25 +4,49 @@ import { Form, Button } from "react-bootstrap";
 import NavBar from "components/NavBar";
 import { UserContext } from "components/auth/UserContext";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { config } from "Constants";
 
 const Login = () => {
+  const [userEmail, setUserEmail] = useState("");
   const [username, setUsername] = useState("");
-  // const { value, setValue } = useContext(UserContext);
-  const { setValue } = useContext(UserContext);
 
+  const { setValue } = useContext(UserContext);
   const history = useHistory();
+  const date = new Date().toISOString();
+
+  const updateLogin = async () => {
+    var result = {};
+    await axios
+      .post(`${config.API_URL}/user/login/update`, {
+        email: userEmail,
+        time: date,
+      })
+      .then((res) => {
+        const group = res.data;
+        result = group;
+        alert("Successfully logged in!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return result;
+  };
 
   /* Functions to handle form submission */
   const handleInputChange = (event) => {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
-
-    setUsername(value);
-    console.log("gelllo");
+    if (target.name === "username") {
+      setUsername(value);
+    } else {
+      setUserEmail(value);
+    }
   };
 
   const handleSubmit = () => {
     setValue(username);
+    updateLogin();
     history.push("/home");
   };
 
@@ -38,6 +62,16 @@ const Login = () => {
             placeholder="John Smith"
             name="username"
             value={username}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="johnsmith@email.com"
+            name="userEmail"
+            value={userEmail}
             onChange={handleInputChange}
           />
         </Form.Group>
