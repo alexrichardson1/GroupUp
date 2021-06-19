0d8a1dfb602a373c016e3873af21b9468587a437
Hussein, Mazen <mah319@ic.ac.uk>
2021-06-19 20:23:17 +0100

    feat(DetailedGroup.js): Add modal for contact details

--- frontend/src/components/DetailedGroup.js
+++ frontend/src/components/DetailedGroup.js
@@ -2,16 +2,18 @@ import React, { useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Button, ListGroup, Table } from "react-bootstrap";
+import { Button, ListGroup, Table, Modal } from "react-bootstrap";
 import NavBar from "components/NavBar";
 import axios from "axios";
 import { config } from "Constants";
 import { UserContext } from "components/auth/UserContext";
+import ContactDetails from "components/ContactDetails";
 
 const DetailedGroup = () => {
   const { id } = useParams();
   const groupId = parseInt(id);
   const history = useHistory();
+  const [email, setEmail] = useState("");
   const [leader, setLeader] = useState("");
   const [maxMembers, setMaxMembers] = useState(0);
   const [teammates, setTeammates] = useState([]);
@@ -19,7 +21,14 @@ const DetailedGroup = () => {
   const [adRequirements, setAdRequirements] = useState("");
   const [projectId, setProjectId] = useState(0);
   const [project, setProject] = useState([]);
+  const [show, setShow] = useState(false);
+  const hide = () => {
+    setShow(false);
+  };
   const { value } = useContext(UserContext);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   console.log(maxMembers, teammates.length);
   const leaderFirstName = leader.split(" ")[0];
@@ -58,6 +67,7 @@ const DetailedGroup = () => {
           setRequirements(group.requirements);
           setAdRequirements(group.adrequirements);
           setProjectId(group.projectid);
+          setEmail(group.leaderemail);
         })
         .catch((error) => {
           console.log(error);
@@ -94,6 +104,21 @@ const DetailedGroup = () => {
         id={projectId}
         create={false}
       />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Contact Details</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Leader Email: {email}.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <h1>{leaderFirstName}'s Group</h1>
       <div>1 members needed.</div>
       <Table striped bordered hover size="sm">
@@ -135,6 +160,7 @@ const DetailedGroup = () => {
         </div>
       )}
       <Button onClick={() => history.goBack()}>Go Back</Button>
+      <Button onClick={handleShow}>Contact Leader</Button>
       <Button
         onClick={() => {
           if (value) joinGroup();
