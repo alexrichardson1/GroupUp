6c9ee1f41cdf006c3ea1c20a551a969956e2ec5c
Hussein, Mazen <mah319@ic.ac.uk>
2021-06-19 14:12:37 +0100

    feat(addProject.sql): Add project sql file

--- backend/sql/addProject.sql
+++ backend/sql/addProject.sql
@@ -1,3 +1,3 @@
-INSERT INTO "projects" ("id", "name", "requirements")
-VALUES (${id}, ${name}, ${requirements})
+INSERT INTO "projects" ("name", "requirements", "description", "hours", "date", "location")
+VALUES (${name}, ${requirements}, ${description}, ${hours}, ${date}, ${location})
 RETURNING *
