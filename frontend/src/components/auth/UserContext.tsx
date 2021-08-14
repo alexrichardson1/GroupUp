fd361453cb8d5b1f38929f1bd2afb2685c81b2ec
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-14 12:29:05 +0100

    fix(UserContex.tsx): Define `UserContextT`

--- frontend/src/components/auth/UserContext.tsx
+++ frontend/src/components/auth/UserContext.tsx
@@ -1,5 +1,17 @@
+import { dummyUser } from "api";
 import { createContext } from "react";
+import { UserT } from "types/types";
 
-export const UserContext = createContext(null);
+interface UserContextT {
+  email: string;
+  user: UserT;
+  setEmail: (email: string) => void;
+  setUser: (user: UserT) => void;
+}
 
-export default UserContext;
+export const UserContext = createContext<UserContextT>({
+  email: dummyUser.email,
+  user: dummyUser,
+  setEmail: (_email) => console.warn("No email"),
+  setUser: (_user) => console.warn("No user."),
+});
