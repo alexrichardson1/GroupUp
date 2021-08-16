9ac8dec757ff2aa62f9da82207e74964ded5db6c
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-16 09:58:33 +0100

    style(UserContext.tsx): Add comment

--- frontend/src/components/auth/UserContext.tsx
+++ frontend/src/components/auth/UserContext.tsx
@@ -3,6 +3,7 @@ import { createContext } from "react";
 
 interface UserContextT {
   email: string;
+  // name of user
   user: string;
   setEmail: (email: string) => void;
   setUser: (user: string) => void;
@@ -14,16 +15,3 @@ export const UserContext = createContext<UserContextT>({
   setEmail: (_email) => console.warn("No email"),
   setUser: (_user) => console.warn("No user."),
 });
-
-/*
-export type ThemeContextType = {
-    theme: Theme;
-    setTheme: (Theme: Theme) => void;
-}
-
-export const ThemeContext = createContext<ThemeContextType>({ 
-  theme: Theme.Dark, 
-  setTheme: theme => console.warn('no theme provider')});
-  
-export const useTheme = () => useContext(ThemeContext);
-*/
